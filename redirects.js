4d07b3652dae9ee43d7cec3798d715a14c4869c9
EnPalabras <it@enpalabras.com.ar>
2026-08-28 16:34:50 -0300

    Set final destinations for journal-embarazo-1/2

--- redirects.js
+++ redirects.js
@@ -1,5 +1,5 @@
 export const REDIRECTS = {
   test: "https://enpalabras.com.ar",
-  "journal-embarazo-1": "https://example.com",
-  "journal-embarazo-2": "https://example.com",
+  "journal-embarazo-1": "https://open.spotify.com/playlist/1GUzYl2Yi6koHAi7w8MWvS?si=x0sfTjG_Q3qBd-FIktlOIQ",
+  "journal-embarazo-2": "https://instagram.com/enpalabrass",
 };
